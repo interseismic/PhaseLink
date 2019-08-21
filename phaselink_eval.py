88072ed334751b6b2f08c78b81b5dc4642fb30e0
Zach Ross <zross@gps.caltech.edu>
2019-08-21 20:14:10 +0900

    Add files via upload

--- phaselink_eval.py
+++ phaselink_eval.py
@@ -80,46 +80,8 @@ def permute_seq(X, t_win, n_max_picks):
 
     return X0
 
-def link_phases(Y, sncl_map, n_min_nucl, n_min_merge):
-    clusters = []
-    sncls = []
-    master_pool = set()
-    for i in range(Y.shape[0]-1, -1, -1):
-        idx = np.where(Y[i,:] == 1)[0]
-        if idx.size < n_min_nucl:
-            continue
-        idx += i
-        idx_set = set(idx)
-        sncls_set = set([(x, sncl_map[x]) for x in idx if x < len(sncl_map)])
-        if len(master_pool.intersection(idx_set)) == 0:
-            clusters.append(idx_set)
-            sncls.append(sncls_set)
-            master_pool.update(idx_set)
-        else:
-            best = 0
-            n_common = []
-            for cluster in clusters:
-                count = len(cluster.intersection(idx_set))
-                n_common.append(count)
-            best = np.argmax(n_common)
-            if n_common[best] < n_min_merge:
-                continue
 
-            # BEGIN SECTION FOR PREVENTING DUPLICATES
-            #others = sncls[best].symmetric_difference(sncls_set)
-            #other_sncls = set([x[1] for x in others])
-            #count_sncl = len(others) - len(other_sncls)
-            #if count_sncl > 0:
-            #    continue
-            # END SECTION FOR PREVENTING DUPLICATES
-
-            clusters[best].update(idx_set)
-            sncls[best].update(sncls_set)
-            master_pool.update(idx_set)
-    return clusters
-
-
-def link_phases_3(Y, n_min_nucl, n_min_merge):
+def link_phases(Y, n_min_nucl, n_min_merge):
     clusters = []
 
     for i in range(Y.shape[0]):
@@ -149,143 +111,145 @@ def link_phases_3(Y, n_min_nucl, n_min_merge):
 
     return clusters
 
-def link_phases_alt(Y, times, n_min_nucl, n_min_merge):
-    clusters = []
+def proba_error_map(pr):
+    if (pr >= 0.5) and (pr < 0.6):
+        return 1.0
+    elif (pr >= 0.6) and (pr < 0.7):
+        return 0.5
+    elif (pr >= 0.7) and (pr < 0.8):
+        return 0.25
+    elif (pr >= 0.8) and (pr < 0.9):
+        return 0.10
+    elif (pr >= 0.9) and (pr < 0.95):
+        return 0.05
+    elif (pr >= 0.95):
+        return 0.02
 
-    # First nucleate all initial clusters
-    for i in range(Y.shape[0]-1, -1, -1):
-
-        idx = np.where(Y[i,:] == 1)[0]
-        if idx.size < n_min_nucl:
-            continue
-        idx += i
-        idx_set = set(idx)
-        clusters.append(set(idx))
-
-    import time
-    t0 = time.time()
-    print("Begin cluster analysis:")
-    print("Iter. 0: %d clusters" % len(clusters))
-    count = 1
-
-    while True:
-
-        # For each cluster calculate number in common
-        n_common = np.zeros((len(clusters), len(clusters)))
-
-        #for i, cl1 in enumerate(clusters):
-        #    for j, cl2 in enumerate(clusters):
-        #        if i >= j:
-        #            continue
-        #        for i1 in cl1: break
-        #        for i2 in cl2: break
-        #        t1 = times[i1]
-        #        t2 = times[i2]
-        #        if abs(t1-t2) >= 60.0:
-        #            continue
-        #        n_common[i,j] = len(cl1.intersection(cl2))
-
-        best = np.unravel_index(n_common.argmax(), n_common.shape)
-
-        # Break condition: check that best has at least n_min_merge in common
-        if n_common[best[0], best[1]] < n_min_merge:
-            break
-
-        clusters[best[0]].update(clusters[best[1]])
-        del clusters[best[1]]
-
-        print("Iter %d: %d clusters, %d in common for best (%.2f sec)" % \
-             (count, len(clusters), n_common[best[0], best[1]],
-              time.time()-t0))
-        t0 = time.time()
-        count += 1
-
-    return clusters
-
-def detect_events(X, labels, model, params):
-    X[:,2] -= X[0,2]
-    sncl_map = X[:,5]
-    trig_pr = X[:,6]
-    X = X[:,:5]
-
-    # Permute pick matrix for all lags and drop picks more than t_win outside
-    print("Permuting sequence for all lags...")
-    X_perm = torch.from_numpy(permute_seq(X, params['t_win'],
-        params['n_max_picks'])).float()
-    print("Finished permuting sequence")
-
-    # Predict association labels for all windows
-    Y_pred = torch.zeros((X_perm.size(0), X_perm.size(1), 1)).float()
-    print("Predicting labels for all phases")
-    for i in range(0, Y_pred.shape[0], params['batch_size']):
-        i_start = i
-        i_stop = i + params['batch_size']
-        if i_stop > Y_pred.shape[0]:
-            i_stop = Y_pred.shape[0]
-        X_test = X_perm[i_start:i_stop]
-        X_test = X_test.cuda(device)
-        Y_pred[i_start:i_stop] = model(X_test).detach()
-    Y_pred = Y_pred.view(Y_pred.size(0), Y_pred.size(1))
-    print("Finished label prediction")
-
-    Y0 = torch.round(Y_pred).numpy()
-
-    print("Linking phases")
-    clusters = link_phases_3(Y0, params['n_min_nucl'], params['n_min_merge'])
-    print("%d events detected initially" % len(clusters))
-
-    print("Removing duplicates")
-    count = 0
-    for i, cluster in enumerate(clusters):
-        phases = {}
-        for idx in cluster:
-            if idx >= len(labels):
-                continue
-            net, sta, phase, time = labels[idx].split()
-            if (net, sta, phase) not in phases:
-                phases[(net, sta, phase)] = [(idx, trig_pr[idx])]
-            else:
-                phases[(net, sta, phase)].append((idx, trig_pr[idx]))
-        for key in phases:
-            if len(phases[key]) > 1:
-                sorted(phases[key], key=lambda x: x[1])
-                phases[key] = [phases[key][-1]]
-        if len(phases) >= params['n_min_det']:
-            count += 1
-        clusters[i] = [phases[key][0][0] for key in phases]
-    print("%d events detected after duplicate removal" % len(clusters))
-
-    clusters = [x for x in clusters if len(x) >= params['n_min_det']]
-
-    # Plot results
-    print("%d events left after applying n_min_det threshold" % len(clusters))
+def detect_events(X, Y, model, params):
 
     ofile = open(params['outfile'], 'w')
-    events = []
-    for i, cluster in enumerate(clusters):
-        idx = np.array(list(cluster))
-        #phases = set()
 
-        event = Event()
-        for j in idx:
-            net, sta, phase, time = labels[j].split()
-            time = UTCDateTime(time)
-            arrival = Arrival(net, sta, time, phase)
-            event.arrivals.append(arrival)
+    X[:,2] -= X[0,2]
+    trig_pr_full = X[:,5]
+    X = X[:,:5]
 
-            ofile.write("%-6s %-4s %-4s %-1s %-6s %-1s "
-                        "%04d%02d%02d %02d%02d "
-                        "%7.4f %-3s %9.2e %9.2e %9.2e %9.2e\n"
-                % (sta, "?", "?", "?", phase, "?", time.year, time.month,
-                   time.day, time.hour, time.minute,
-                   time.second+time.microsecond/1.e6, "GAU", 0, 0, 0, 0))
-        ofile.write("\n")
-        events.append(event)
+    n_cumul_dets = 0
+
+    for t_start in np.arange(0, X[-1,2], 86400.):
+
+        t_stop = t_start + 86400.
+
+        idx = np.where((X[:,2] >= t_start) & (X[:,2] < t_stop))[0]
+
+        print(t_start/86400., t_stop/86400., idx.size, n_cumul_dets)
+
+        if idx.size == 0:
+            continue
+
+        labels = [Y[x] for x in idx]
+        trig_pr = trig_pr_full[idx]
+
+        # Permute pick matrix for all lags 
+        print("Permuting sequence for all lags...")
+        X_perm = permute_seq(X[idx], params['t_win'], params['n_max_picks'])
+        X_perm = torch.from_numpy(X_perm).float()
+        print("Finished permuting sequence")
+
+        # Predict association labels for all windows
+        Y_pred = torch.zeros((X_perm.size(0), X_perm.size(1), 1)).float()
+        Y_pred = Y_pred.to(device)
+        print("Predicting labels for all phases")
+        for i in range(0, Y_pred.shape[0], params['batch_size']):
+            i_start = i
+            i_stop = i + params['batch_size']
+            if i_stop > Y_pred.shape[0]:
+                i_stop = Y_pred.shape[0]
+            X_test = X_perm[i_start:i_stop]
+            X_test = X_test.cuda(device)
+            with torch.no_grad():
+                Y_pred[i_start:i_stop] = model(X_test)
+        Y_pred = Y_pred.view(Y_pred.size(0), Y_pred.size(1)).cpu()
+        print("Finished label prediction")
+
+        Y0 = torch.round(Y_pred).numpy()
+
+        print("Linking phases")
+        clusters = link_phases(Y0, params['n_min_nucl'], params['n_min_merge'])
+        print("%d events detected initially" % len(clusters))
+
+        print("Removing duplicates")
+        count = 0
+        for i, cluster in enumerate(clusters):
+            phases = {}
+            for idx in cluster:
+                if idx >= len(labels):
+                    continue
+                net, sta, phase, time = labels[idx].split()
+                if (net, sta, phase) not in phases:
+                    phases[(net, sta, phase)] = [(idx, trig_pr[idx])]
+                else:
+                    phases[(net, sta, phase)].append((idx, trig_pr[idx]))
+            for key in phases:
+                if len(phases[key]) > 1:
+                    sorted(phases[key], key=lambda x: x[1])
+                    phases[key] = [phases[key][-1]]
+            if len(phases) >= params['n_min_det']:
+                count += 1
+            clusters[i] = [phases[key][0][0] for key in phases]
+        print("%d events detected after duplicate removal" % len(clusters))
+
+        clusters = [x for x in clusters if len(x) >= params['n_min_det']]
+
+        # Plot results
+        print("%d events left after applying n_min_det threshold" % \
+              len(clusters))
+
+        n_cumul_dets += len(clusters)
+
+        events = []
+        for i, cluster in enumerate(clusters):
+            idx = np.array(list(cluster))
+
+            event = Event()
+            for j in idx:
+                net, sta, phase, time = labels[j].split()
+                proba = trig_pr[j]
+                pick_error = proba_error_map(proba)
+                time = UTCDateTime(time)
+                arrival = Arrival(net, sta, time, phase)
+                event.arrivals.append(arrival)
+
+                ofile.write(
+                    "%-6s %-4s %-4s %-1s %-6s %-1s "
+                    "%04d%02d%02d %02d%02d "
+                    "%7.4f %-3s %9.2e %9.2e %9.2e %9.2e\n" % \
+                    (
+                        sta,
+                        net,
+                        "?",
+                        "?",
+                        phase,
+                        "?",
+                        time.year,
+                        time.month,
+                        time.day,
+                        time.hour,
+                        time.minute,
+                        time.second+time.microsecond/1.e6,
+                        "GAU",
+                        pick_error,
+                        -1,
+                        -1,
+                        -1,
+                    )
+                )
+            ofile.write("\n")
+            events.append(event)
     ofile.close()
 
-    ofile = open(params['outpkl'], 'wb')
-    pickle.dump(events, ofile)
-    ofile.close()
+    #ofile = open(params['outpkl'], 'wb')
+    #pickle.dump(events, ofile)
+    #ofile.close()
 
 def read_gpd_output(params):
     from obspy import UTCDateTime
@@ -311,10 +275,10 @@ def read_gpd_output(params):
                     missing_sta_list.add((net, sta))
                 continue
             otime = UTCDateTime("%s" % time) - UTCDateTime(0)
-            sncl_idx = sncl_map[(net, sta, phase)]
-            X.append((sta_X, sta_Y, otime, phase_idx[phase],
-                      1, sncl_idx, float(pr)))
+            X.append((sta_X, sta_Y, otime, phase_idx[phase], 1, float(pr)))
             outputs.append("%s %s %s %s" % (net, sta, phase, time))
+            if len(X) % 100000 == 0:
+                print(len(X))
     X = np.array(X)
     idx = np.argsort(X[:,2])
     X = X[idx,:]
--- phaselink_train.py
+++ phaselink_train.py
@@ -6,6 +6,7 @@ import torch.utils.data
 import sys
 import json
 import pickle
+import apex.amp as amp
 
 n_epochs = 20
 
@@ -27,7 +28,6 @@ class MyDataset(torch.utils.data.Dataset):
     def __len__(self):
         return len(self.data)
 
-
 class StackedGRU(torch.nn.Module):
     def __init__(self):
         super(StackedGRU, self).__init__()
@@ -40,7 +40,7 @@ class StackedGRU(torch.nn.Module):
             batch_first=True, bidirectional=True)
         self.gru2 = torch.nn.GRU(self.hidden_size*2, self.hidden_size, \
             batch_first=True, bidirectional=True)
-        self.sigmoid = torch.nn.Sigmoid()
+        #self.sigmoid = torch.nn.Sigmoid()
         #self.dropout = torch.nn.Dropout(p=0.25)
 
     def forward(self, inp):
@@ -55,187 +55,8 @@ class StackedGRU(torch.nn.Module):
         out = self.gru2(h_t)
         h_t = out[0]
         out = self.fc4(h_t)
-        out = self.sigmoid(out)
+        #out = self.sigmoid(out)
         return out
-
-class Encoder(torch.nn.Module):
-    def __init__(self, input_size, hidden_size):
-        super(Encoder, self).__init__()
-        self.hidden_size = hidden_size
-        self.gru = torch.nn.GRU(input_size, self.hidden_size)
-
-    def forward(self, input):
-        output, hidden = self.gru(input)
-        return output, hidden
-
-class AttnDecoder(torch.nn.Module):
-    def __init__(self, input_size, hidden_size, output_size,
-            max_length):
-        super(AttnDecoder, self).__init__()
-        self.hidden_size = hidden_size
-        self.output_size = output_size
-        self.max_length = max_length
-
-        # Layers
-        self.attn = torch.nn.Linear(self.hidden_size + 1, self.max_length)
-        self.attn_combine = torch.nn.Linear(self.hidden_size + 1,
-            self.hidden_size)
-        self.gru = torch.nn.GRU(self.hidden_size, self.hidden_size)
-        self.out = torch.nn.Linear(self.hidden_size, self.output_size)
-
-
-    def forward(self, input, hidden, encoder_outputs):
-        import torch.nn.functional as F
-        cat_inp = torch.cat((input, hidden), 2)
-        attn_weights = F.softmax(self.attn(cat_inp), dim=2)
-        attn_applied = torch.bmm(attn_weights.permute(1,0,2),
-            encoder_outputs.permute(1,0,2))
-
-        output = torch.cat((input.permute(1,0,2), attn_applied), 2)
-        output = self.attn_combine(output)
-        output = F.relu(output).permute(1,0,2)
-
-        output, hidden = self.gru(output, hidden)
-        output = self.out(output)
-        output = torch.sigmoid(output)
-        return output, hidden
-
-class AttnModel():
-    def __init__(self, encoder, decoder, enc_optimizer, dec_optimizer,
-        model_path):
-        self.encoder = encoder
-        self.decoder = decoder
-        self.enc_optimizer = enc_optimizer
-        self.dec_optimizer = dec_optimizer
-        self.model_path = model_path
-
-    def train(self, train_loader, val_loader, n_epochs):
-        from torch.autograd import Variable
-        import time
-
-        loss = torch.nn.BCELoss()
-        n_batches = len(train_loader)
-        training_start_time = time.time()
-        SOS_token = 0
-
-        for epoch in range(n_epochs):
-            running_loss = 0.0
-            running_acc = 0
-            running_val_acc = 0
-            print_every = n_batches // 10
-            start_time = time.time()
-            total_train_loss = 0
-            total_val_loss = 0
-            total_val_acc = 0
-            running_sample_count = 0
-            loss_value = 0
-
-            for i, data in enumerate(train_loader, 0):
-                # Get inputs/outputs and wrap in variable object
-                inputs, labels = data
-                inputs = inputs.to(device).permute(1, 0, 2)
-                labels = labels.to(device)
-
-                # Set gradients for all parameters to zero
-                self.enc_optimizer.zero_grad()
-                self.dec_optimizer.zero_grad()
-
-                # Forward pass
-                encoder_outputs, encoder_hidden = self.encoder(inputs)
-
-                decoder_hidden = encoder_hidden
-                decoder_input = torch.zeros((1, inputs.shape[1], 1),
-                    device=device)
-
-                outputs = torch.zeros(labels.shape, device=device)
-                for j in range(labels.shape[0]):
-                    decoder_output, decoder_hidden = self.decoder(
-                        decoder_input, decoder_hidden, encoder_outputs)
-
-                    loss_value += loss(decoder_output, labels[:,j].float())
-                    decoder_input = labels[:,j][None,:,None].float().detach()
-                    decoder_output = decoder_output.detach()
-                    outputs[:,j] = decoder_output.squeeze().detach()
-
-                # Backward pass
-                outputs = outputs.view(-1)
-                labels = labels.view(-1)
-                loss_value.backward(retain_graph=True)
-
-
-                # Update parameters
-                self.enc_optimizer.step()
-                self.dec_optimizer.step()
-
-                # Print statistics
-                running_loss += loss_value.data.item()
-                total_train_loss += loss_value.data.item()
-
-                # Calculate categorical accuracy
-                pred = torch.round(outputs).long()
-
-                running_acc += (pred == labels).sum().item()
-                running_sample_count += len(labels)
-
-                # Print every 10th batch of an epoch
-                if (i + 1) % (print_every + 1) == 0:
-                    print("Epoch {}, {:d}% \t train_loss: {:.4e} "
-                        "train_acc: {:4.2f}% took: {:.2f}s".format(
-                        epoch + 1, int(100 * (i + 1) / n_batches),
-                        running_loss / print_every,
-                        100*running_acc / running_sample_count,
-                        time.time() - start_time))
-
-                    # Reset running loss and time
-                    running_loss = 0.0
-                    start_time = time.time()
-
-            running_sample_count = 0
-            y_pred_all, y_true_all = [], []
-            for inputs, labels in val_loader:
-                # Wrap tensors in Variables
-                inputs = inputs.cuda(device)
-                labels = labels.cuda(device)
-
-                # Forward pass only
-                val_outputs = self.network(inputs)
-                val_outputs = val_outputs.view(-1)
-                labels = labels.view(-1)
-                val_loss = loss(val_outputs, labels.float())
-                total_val_loss += val_loss.data.item()
-
-                # Calculate categorical accuracy
-                pred = torch.round(val_outputs).long()
-                running_val_acc += (pred == labels).sum().item()
-                running_sample_count += len(labels)
-
-                y_pred_all.append(pred.cpu().numpy().flatten())
-                y_true_all.append(labels.cpu().numpy().flatten())
-
-            y_pred_all = np.concatenate(y_pred_all)
-            y_true_all = np.concatenate(y_true_all)
-
-            from sklearn.metrics import classification_report
-            print(classification_report(y_true_all, y_pred_all))
-
-            total_val_loss /= len(val_loader)
-            total_val_acc = running_val_acc / running_sample_count
-            print(
-                "Validation loss = {:.4e}   acc = {:4.2f}%".format(
-                    total_val_loss,
-                    100*total_val_acc))
-
-            torch.save({
-                'epoch': epoch,
-                'model_state_dict': self.network.state_dict(),
-                'optimizer_state_dict': self.optimizer.state_dict(),
-                'loss': total_val_loss,
-            }, '%s/model_%03d_%f.pt' % (self.model_path, epoch, total_val_loss))
-
-        print(
-            "Training finished, took {:.2f}s".format(
-                time.time() -
-                training_start_time))
 
 class Model():
     def __init__(self, network, optimizer, model_path):
@@ -247,7 +68,8 @@ class Model():
         from torch.autograd import Variable
         import time
 
-        loss = torch.nn.BCELoss()
+        #loss = torch.nn.BCELoss()
+        loss = torch.nn.BCEWithLogitsLoss()
         n_batches = len(train_loader)
         training_start_time = time.time()
 
@@ -278,55 +100,60 @@ class Model():
                 outputs = outputs.view(-1)
                 labels = labels.view(-1)
                 loss_value = loss(outputs, labels.float())
+                #loss_ = loss(outputs, labels.float())
+                #with amp.scale_loss(loss_, optimizer) as loss_value:
+                #    loss_value.backward()
                 loss_value.backward()
 
                 # Update parameters
                 self.optimizer.step()
 
-                # Print statistics
-                running_loss += loss_value.data.item()
-                total_train_loss += loss_value.data.item()
+                with torch.no_grad():
+                    # Print statistics
+                    running_loss += loss_value.data.item()
+                    total_train_loss += loss_value.data.item()
 
-                # Calculate categorical accuracy
-                pred = torch.round(outputs).long()
+                    # Calculate categorical accuracy
+                    pred = torch.round(torch.sigmoid(outputs)).long()
 
-                running_acc += (pred == labels).sum().item()
-                running_sample_count += len(labels)
+                    running_acc += (pred == labels).sum().item()
+                    running_sample_count += len(labels)
 
-                # Print every 10th batch of an epoch
-                if (i + 1) % (print_every + 1) == 0:
-                    print("Epoch {}, {:d}% \t train_loss: {:.4e} "
-                        "train_acc: {:4.2f}% took: {:.2f}s".format(
-                        epoch + 1, int(100 * (i + 1) / n_batches),
-                        running_loss / print_every,
-                        100*running_acc / running_sample_count,
-                        time.time() - start_time))
+                    # Print every 10th batch of an epoch
+                    if (i + 1) % (print_every + 1) == 0:
+                        print("Epoch {}, {:d}% \t train_loss: {:.4e} "
+                            "train_acc: {:4.2f}% took: {:.2f}s".format(
+                            epoch + 1, int(100 * (i + 1) / n_batches),
+                            running_loss / print_every,
+                            100*running_acc / running_sample_count,
+                            time.time() - start_time))
 
-                    # Reset running loss and time
-                    running_loss = 0.0
-                    start_time = time.time()
+                        # Reset running loss and time
+                        running_loss = 0.0
+                        start_time = time.time()
 
             running_sample_count = 0
             y_pred_all, y_true_all = [], []
-            for inputs, labels in val_loader:
-                # Wrap tensors in Variables
-                inputs = inputs.cuda(device)
-                labels = labels.cuda(device)
+            with torch.no_grad():
+                for inputs, labels in val_loader:
+                    # Wrap tensors in Variables
+                    inputs = inputs.to(device)
+                    labels = labels.to(device)
 
-                # Forward pass only
-                val_outputs = self.network(inputs)
-                val_outputs = val_outputs.view(-1)
-                labels = labels.view(-1)
-                val_loss = loss(val_outputs, labels.float())
-                total_val_loss += val_loss.data.item()
+                    # Forward pass only
+                    val_outputs = torch.sigmoid(self.network(inputs))
+                    val_outputs = val_outputs.view(-1)
+                    labels = labels.view(-1)
+                    val_loss = loss(val_outputs, labels.float())
+                    total_val_loss += val_loss.data.item()
 
-                # Calculate categorical accuracy
-                pred = torch.round(val_outputs).long()
-                running_val_acc += (pred == labels).sum().item()
-                running_sample_count += len(labels)
+                    # Calculate categorical accuracy
+                    pred = torch.round(val_outputs).long()
+                    running_val_acc += (pred == labels).sum().item()
+                    running_sample_count += len(labels)
 
-                y_pred_all.append(pred.cpu().numpy().flatten())
-                y_true_all.append(labels.cpu().numpy().flatten())
+                    y_pred_all.append(pred.cpu().numpy().flatten())
+                    y_true_all.append(labels.cpu().numpy().flatten())
 
             y_pred_all = np.concatenate(y_pred_all)
             y_true_all = np.concatenate(y_true_all)
@@ -361,8 +188,8 @@ class Model():
 
             # Wrap tensors in Variables
             inputs, labels = Variable(
-                inputs.cuda(device)), Variable(
-                labels.cuda(device))
+                inputs.to(device)), Variable(
+                labels.to(device))
 
             # Forward pass only
             val_outputs = self.network(inputs)
@@ -401,28 +228,26 @@ if __name__ == "__main__":
 
     train_loader = torch.utils.data.DataLoader(
         dataset,
-        batch_size=32,
+        batch_size=256,
         shuffle=False,
         sampler=train_sampler
     )
     val_loader = torch.utils.data.DataLoader(
         dataset,
-        batch_size=32,
+        batch_size=256,
         shuffle=False,
         sampler=validation_sampler
     )
 
     stackedgru = StackedGRU()
-    #stackedgru = torch.nn.DataParallel(StackedGRU(), device_ids=['cuda:2', 'cuda:3', 'cuda:4'])
-    stackedgru = stackedgru.cuda(device)
+    stackedgru = stackedgru.to(device)
+    #stackedgru = torch.nn.DataParallel(stackedgru,
+    #    device_ids=['cuda:2', 'cuda:3', 'cuda:4', 'cuda:5'])
     optimizer = torch.optim.Adam(stackedgru.parameters())
-    #enc = Encoder(input_size=4, hidden_size=64).cuda(device)
-    #dec = AttnDecoder(input_size=4, hidden_size=64, output_size=1,
-    #    max_length=500).cuda(device)
-    #enc_opt = torch.optim.Adam(enc.parameters())
-    #dec_opt = torch.optim.Adam(dec.parameters())
+
+    #stackedgru, optimizer = amp.initialize(stackedgru, optimizer,
+    #    opt_level='O1')
 
     model = Model(stackedgru, optimizer, \
-        model_path='./models_saved/stacked_gru/')
-    #model = AttnModel(enc, dec, enc_opt, dec_opt, \
+        model_path='./models_saved/')
     model.train(train_loader, val_loader, n_epochs)
